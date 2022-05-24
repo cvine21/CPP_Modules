f5c4724f5ebb88563e78dd11e078820fca499746
Claudine Vine <cvine@va-h5.kzn.21-school.ru>
2022-05-24 20:58:51 +0300

    minor changes

---
+++ ex02/src/Account.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvine <cvine@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 20:54:24 by cvine             #+#    #+#             */
+/*   Updated: 2022/05/24 20:57:25 by cvine            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit ) {}
+
+Account::~Account( void ) {}
+
+void	Account::makeDeposit( int deposit ) {};
+
+bool	Account::makeWithdrawal( int withdrawal ) {};
+
+int		Account::checkAmount( void ) const {};
+
+void	Account::displayStatus( void ) const {};
