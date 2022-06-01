0aa626fb4fbf27f536cdb053772d060e1d1abad2
Claudine Vine <cvine@va-j5.kzn.21-school.ru>
2022-06-01 12:04:15 +0300

    cpp04/ex00 finished

---
+++ cpp04/ex00/include/Animal.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvine <cvine@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 11:22:36 by cvine             #+#    #+#             */
+/*   Updated: 2022/06/01 12:02:14 by cvine            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+
+// ************************************************************************** //
+//                                Animal Class                                //
+// ************************************************************************** //
+
+class Animal {
+
+	public:
+
+		Animal( void );
+		Animal( std::string );
+		Animal( Animal const& );
+		~Animal( void );
+		Animal & operator = ( Animal const& );
+
+		std::string	getType( void ) const;
+
+		virtual void	makeSound( void ) const;
+
+	protected:
+	
+		std::string	_type;
+
+};
+
+#endif /* __ANIMAL_HPP__ */
---
+++ cpp04/ex00/src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvine <cvine@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 11:36:55 by cvine             #+#    #+#             */
+/*   Updated: 2022/06/01 11:50:35 by cvine            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+#include "Dog.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* dog = new Dog();
+	const Animal* cat = new Cat();
+
+	std::cout << dog->getType() << " " << std::endl;
+	std::cout << cat->getType() << " " << std::endl;
+	cat->makeSound(); //will output the cat sound!
+	dog->makeSound();
+	meta->makeSound();
+	return 0;
+}
