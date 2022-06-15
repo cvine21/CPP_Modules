d5fbf3362fff90f6474229a901e61206d292c5e6
Claudine Vine <cvine@va-d3.kzn.21-school.ru>
2022-06-15 15:22:20 +0300

    fix: cpp06/ex00 segfault when no args

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cvine <cvine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 15:42:44 by cvine             #+#    #+#             */
-/*   Updated: 2022/06/14 14:14:19 by cvine            ###   ########.fr       */
+/*   Updated: 2022/06/15 15:20:46 by cvine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int main(int argc, char **argv) {
 
+	if (argc != 2) {
+		std::cout << "Invalid number of arguments" << std::endl;
+		return 0;
+	}
+	
 	String	str(argv[1]);
 	char	c;
 	int		i;
 	float	f;
 	double	d;
-
-	if (argc != 2) {
-		std::cout << "Invalid number of arguments";
-		return 0;
-	}
 	
 	c = static_cast<char>(str);
 	i = static_cast<int>(str);
